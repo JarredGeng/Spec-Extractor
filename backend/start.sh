586da4183825f3a05302bcde3eda0f77cac488e8
JarredGeng <160256580+JarredGeng@users.noreply.github.com>
2025-07-11 12:01:53 -0400

    chage

--- backend/start.sh
+++ backend/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+export PLAYWRIGHT_BROWSERS_PATH=0  # forces install to local .playwright directory
+
 echo "Installing Playwright browsers..."
-playwright install --with-deps
+playwright install chromium
 
 echo "Starting Flask app..."
 python app.py
